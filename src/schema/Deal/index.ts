6633809fa2bf9fd8f7c8d97c6e1a8573eb1c8cdd
jimmg35 <jim60308@gmail.com>
2022-09-16 18:09:10 +0800

    新增註解

--- src/schema/Deal/index.ts
+++ src/schema/Deal/index.ts
@@ -6,41 +6,41 @@ interface IParsedValue<T> {
 }
 
 export interface IDeal {
-  town: string                      // Deprecate
-  transactionTarget: string         // Deprecate
-  address: string                   // Deprecate
-  landTransferArea: string          // Done
-  urbanLandUse: string              // Done
-  nonUrbanLandUse: string           // Done
-  nonUrbanLandUsePlanning: string   // Done
-  transactionTime: string           // Done
-  transactionAmount: string         // Done
-  transferFloor: string             // Done
-  floor: string                     // Done
-  buildingType: string              // Done
-  usage: string                     // Deprecate
-  buildingMaterial: string          // Deprecate
-  completionTime: string            // Done
-  buildingTransferArea: string      // Done
-  roomNumber: string                // Done
-  hallNumber: string                // Done
-  bathNumber: string                // Done
-  hasCompartment: string            // Done
-  hasCommittee: string              // Done
-  price: string                     // Done
-  unitPrice: string                 // Done
-  parkingSpaceType: string
-  parkingSpaceTransferArea: string  // Done
-  parkingSpacePrice: string         // Done
-  coordinate_y: string              // Done
-  coordinate_x: string              // Done
-  hasNotes: string                  // Deprecate
-  notes: string
-  id: string                        // Done
-  buildingArea: string              // Done
-  subBuildingArea: string           // Done
-  belconyArea: string               // Done
-  hasElevator: string               // Done
+  town: string                      // 鄉鎮市區(Deprecate)
+  transactionTarget: string         // 交易標的(Deprecate)
+  address: string                   // 土地位置/建物門牌(Deprecate)
+  landTransferArea: string          // 土地移轉總面積(平方公尺)
+  urbanLandUse: string              // 都市土地使用分區
+  nonUrbanLandUse: string           // 非都市土地使用分區
+  nonUrbanLandUsePlanning: string   // 非都市土地使用編定
+  transactionTime: string           // 交易年月日
+  transactionAmount: string         // 交易筆棟數
+  transferFloor: string             // 移轉層次
+  floor: string                     // 總樓層數
+  buildingType: string              // 建物型態
+  usage: string                     // 主要用途(Deprecate)
+  buildingMaterial: string          // 主要建材(Deprecate)
+  completionTime: string            // 建築完成年月
+  buildingTransferArea: string      // 建物移轉總面積(平方公尺)
+  roomNumber: string                // 建物現況格局-房
+  hallNumber: string                // 建物現況格局-廳
+  bathNumber: string                // 建物現況格局-衛
+  hasCompartment: string            // 建物現況格局-隔間
+  hasCommittee: string              // 有無管理組織
+  price: string                     // 總價(元)
+  unitPrice: string                 // 單價(元/平方公尺)
+  parkingSpaceType: string          // 車位類別
+  parkingSpaceTransferArea: string  // 車位移轉總面積(平方公尺)
+  parkingSpacePrice: string         // 車位總價(元)
+  coordinate_y: string              // 交易標的橫坐標
+  coordinate_x: string              // 交易標的縱坐標
+  hasNotes: string                  // 有無備註欄(Y/N) (Deprecate)
+  notes: string                     // 備註
+  id: string                        // 編號
+  buildingArea: string              // 主建物面積
+  subBuildingArea: string           // 附屬建物面積
+  belconyArea: string               // 陽台面積
+  hasElevator: string               // 電梯
   parsedValue: {
     transactionTime?: IParsedValue<string | undefined>
     completionTime?: IParsedValue<string | undefined>
