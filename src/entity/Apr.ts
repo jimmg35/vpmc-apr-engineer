40e8c3f96683295f97237ceb2ee08e32ca4bc1a1
jimmg35 <jim60308@gmail.com>
2023-10-30 09:03:14 +0800

    feat: yeah

--- src/entity/Apr.ts
+++ src/entity/Apr.ts
@@ -6,12 +6,114 @@ import {
 } from "typeorm"
 import { Point } from "geojson"
 import { buildingType, hasCommittee, hasCompartment, hasElevator, nonUrbanLandUse, parkingSpaceType, urbanLandUse } from "../_function/deal/enum"
+import { StringChain } from "lodash"
 
 @Entity({ name: 'deal' })
 export class Deal {
 
     @PrimaryGeneratedColumn('uuid')
     id: string
+
+
+    @Column({ type: 'text' })
+    town_raw: string
+
+    @Column({ type: 'text' })
+    transactionTarget_raw: string
+
+    @Column({ type: 'text' })
+    address_raw: string
+
+    @Column({ type: 'text' })
+    landTransferArea_raw: string
+
+    @Column({ type: 'text' })
+    urbanLandUse_raw: string
+
+    @Column({ type: 'text' })
+    nonUrbanLandUse_raw: string
+
+    @Column({ type: 'text' })
+    nonUrbanLandUsePlanning_raw: string
+
+    @Column({ type: 'text' })
+    transactionTime_raw: string
+
+    @Column({ type: 'text' })
+    transactionAmount_raw: string
+
+    @Column({ type: 'text' })
+    transferFloor_raw: string
+
+    @Column({ type: 'text' })
+    floor_raw: string
+
+    @Column({ type: 'text' })
+    buildingType_raw: string
+
+    @Column({ type: 'text' })
+    usage_raw: string
+
+    @Column({ type: 'text' })
+    buildingMaterial_raw: string
+
+    @Column({ type: 'text' })
+    completionTime_raw: string
+
+    @Column({ type: 'text' })
+    buildingTransferArea_raw: string
+
+    @Column({ type: 'text' })
+    roomNumber_raw: string
+
+    @Column({ type: 'text' })
+    hallNumber_raw: string
+
+    @Column({ type: 'text' })
+    bathNumber_raw: string
+
+    @Column({ type: 'text' })
+    hasCompartment_raw: string
+
+    @Column({ type: 'text' })
+    hasCommittee_raw: string
+
+    @Column({ type: 'text' })
+    price_raw: string
+
+    @Column({ type: 'text' })
+    unitPrice_raw: string
+
+    @Column({ type: 'text' })
+    parkingSpaceType_raw: string
+
+    @Column({ type: 'text' })
+    parkingSpaceTransferArea_raw: string
+
+    @Column({ type: 'text' })
+    parkingSpacePrice_raw: string
+
+    @Column({ type: 'text' })
+    hasNotes_raw: string
+
+    @Column({ type: 'text' })
+    notes_raw: string
+
+    @Column({ type: 'text' })
+    id_raw: string
+
+    @Column({ type: 'text' })
+    buildingArea_raw: string
+
+    @Column({ type: 'text' })
+    subBuildingArea_raw: string
+
+    @Column({ type: 'text' })
+    belconyArea_raw: string
+
+    @Column({ type: 'text' })
+    hasElevator_raw: string
+
 
     @Column({ type: 'text', unique: true })
     aprId: string
@@ -158,6 +260,42 @@ export class Deal {
 }
 
 export interface IApr {
+    town_raw: string
+    transactionTarget_raw: string
+    address_raw: string
+    landTransferArea_raw: string
+    urbanLandUse_raw: string
+    nonUrbanLandUse_raw: string
+    nonUrbanLandUsePlanning_raw: string
+    transactionTime_raw: string
+    transactionAmount_raw: string
+    transferFloor_raw: string
+    floor_raw: string
+    buildingType_raw: string
+    usage_raw: string
+    buildingMaterial_raw: string
+    completionTime_raw: string
+    buildingTransferArea_raw: string
+    roomNumber_raw: string
+    hallNumber_raw: string
+    bathNumber_raw: string
+    hasCompartment_raw: string
+    hasCommittee_raw: string
+    price_raw: string
+    unitPrice_raw: string
+    parkingSpaceType_raw: string
+    parkingSpaceTransferArea_raw: string
+    parkingSpacePrice_raw: string
+
+    hasNotes_raw: string
+    notes_raw: string
+    id_raw: string
+    buildingArea_raw: string
+    subBuildingArea_raw: string
+    belconyArea_raw: string
+    hasElevator_raw: string
+
+
     id: string
     transactionTime: Date
     completionTime: Date
